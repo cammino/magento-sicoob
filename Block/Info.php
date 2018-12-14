7530dceb2eeeefc27fc919e8e25d5b12102be21d
Marcelo Ferracioli <marceloferracioli@gmail.com>
2018-12-14 11:04:59 -0200

    Adiciona métodos para pegar dados do pedido no bloco de Info.

--- Block/Info.php
+++ Block/Info.php
@@ -5,4 +5,13 @@ class Cammino_Sicoob_Block_Info extends Mage_Payment_Block_Info {
         parent::_construct();
         $this->setTemplate('sicoob/info.phtml');
     }
+
+    public function getOrder() {
+        return $this->getInfo()->getOrder();
+    }
+
+    public function getOrderId() {
+        return $this->getOrder()->getRealOrderId();
+    }
+
 }
